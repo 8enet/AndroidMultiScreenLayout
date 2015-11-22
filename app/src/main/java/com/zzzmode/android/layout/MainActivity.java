10c24885ef17f702504806b2c2c0a6a5e2eb8d1e
8enet <zlcn2200@yeah.net>
2015-11-22 17:30:17 +0800

    change sdk version

--- app/src/main/java/com/zzzmode/android/layout/MainActivity.java
+++ app/src/main/java/com/zzzmode/android/layout/MainActivity.java
@@ -16,12 +16,10 @@
 
 package com.zzzmode.android.layout;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-import com.zzzmode.android.library.LayoutSizeCompat;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
